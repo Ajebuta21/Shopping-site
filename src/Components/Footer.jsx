c579686c8d9ee9ec7295adeeb302b6529cc50923
Ajebuta21 <elvisluckyest@gmail.com>
2023-03-28 15:48:55 +0100

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full text-xl flex justify-center items-center py-4 bg-gray-200 gap-2">
+    <footer className="w-full text-md flex justify-center items-center py-4 bg-gray-200 gap-2">
           © 2023 Copyright: <a href="https://github.com/Ajebuta21">Elvis Lucky</a>
     </footer>
   );
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,15 +4,15 @@ import {AiOutlineSearch} from 'react-icons/ai'
 const Search = () => {
   return (
     <div className="fixed w-full h-[6vh] flex top-[8vh] justify-center items-center z-20 bg-white shadow-sm">
-      <div className="w-2/5 max-lg:w-4/5 h-4/5 rounded-3xl flex items-center transition ease-in-out">
+      <div className="w-2/5 max-lg:w-4/5 h-4/5 flex items-center transition ease-in-out">
         <input
           type="search"
           name=""
           id=""
-          className="flex w-10/12 h-full rounded-tl-3xl rounded-bl-3xl rounded-tr-none rounded-br-none outline-none pl-3 text-xs text-sky-500 border-l border-t border-b focus:border-sky-600"
+          className="flex w-10/12 h-full  outline-none pl-3 text-xs text-sky-500 border-l border-t border-b focus:border-sky-600"
           placeholder="Search here..."
         />
-        <button className="bg-sky-600 text-white w-2/12 h-full rounded-tr-3xl rounded-br-3xl flex justify-center items-center border border-sky-600">
+        <button className="bg-sky-600 text-white w-2/12 h-full  flex justify-center items-center border border-sky-600 lg:hover:opacity-50 transition-colors ease-in-out">
           <AiOutlineSearch />
         </button>
       </div>
